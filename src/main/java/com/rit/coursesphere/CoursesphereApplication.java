3f5da4b5dbf0503657ac6c7f6bc7400e89744c4b
yag1450 <yag1450@g.rit.edu>
2026-04-24 21:34:53 +0400

    initialized Spring Boot project with JPA and MariaDB datasource

---
+++ src/main/java/com/rit/coursesphere/CoursesphereApplication.java
@@ -0,0 +1,13 @@
+package com.rit.coursesphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesphereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rit/coursesphere/CoursesphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rit.coursesphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
